986cd3cfdb8725c7b0be7221933e26bf054bb7a2
hanifnugrahaa <hanifardiyanta11@gmail.com>
2025-07-11 09:07:31 +0700

    build portfolio

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: '/',
-})
+  base: '/', 
+  build: {
+    outDir: 'docs' // hasil build masuk ke folder 'docs'
+  }
+});

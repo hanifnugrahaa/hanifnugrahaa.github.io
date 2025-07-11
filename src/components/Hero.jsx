7ce4548d638e084a0d9dac464a3f16b2d7faa77c
hanifnugrahaa <hanifardiyanta11@gmail.com>
2025-07-11 11:29:53 +0700

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         <h1 className="hero__title">Hi, I'm Hanif Nugraha.</h1>
         <p className="hero__subtitle">I build things for the web & IoT.</p>
         <p className="hero__description">
-          An Electronics and Instrumentation student with a passion for developing innovative digital solutions, ranging from intuitive web interfaces to integrated IoT systems.
+          An Electronics and Instrumentation (UGM) student with a passion for developing innovative digital solutions, ranging from intuitive web interfaces to integrated IoT systems.
         </p>
         <a href="#projects" className="btn btn--hero">View My Projects</a>
       </div>

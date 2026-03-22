85510171f10110857728ff3625f4e87e86efb79c
hanifnugrahaa <hanifardiyanta11@gmail.com>
2026-03-22 19:33:14 +0700

    update: about.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,17 +5,21 @@ import React from "react";
 function About() {
   return (
     <>
-    <h2 className="section-title">About</h2>
+      <h2 className="section-title">About</h2>
       <p>
         An Electronics and Instrumentation student with dual expertise in hardware and software engineering. I translate user needs into tangible solutions through Web Development and the Internet of Things (IoT). My main focus is to create seamlessly integrated systems with user experience as the highest priority. Ready to apply my fast-learning abilities in a dynamic and innovative environment.
       </p>
       <div className="button-container">
-        {/* Path menunjuk ke folder public */}
-        <a href="/assets/docs/CV_Hanif Nugraha.pdf" download className="btn">
-          Download CV
+        <a 
+          href="/assets/docs/CV_Hanif Nugraha.pdf" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="btn"
+        >
+          View CV
         </a>
       </div>
-      </>
+    </>
   );
 }
 
